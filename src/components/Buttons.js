3f3bcf9b0f56ba1ac767259f23f785ea8ec8cc78
unknown <alaashaher12@gmail.com>
2018-10-23 21:17:13 +0200

    optamization

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
+import Button from './Button';
 
-class Buttons extends Component {
-  render() {
-    return <div className="Buttons"> {this.props.children} </div>
-  }
-}
+const Buttons = ({handleClick,...rest}) => (
+    <div className="Buttons">
+    <Button onClick={handleClick} label="C" value="clear" />
+    <Button onClick={handleClick} label="7" value="7" />
+    <Button onClick={handleClick} label="4" value="4" />
+    <Button onClick={handleClick} label="1" value="1" />
+    <Button onClick={handleClick} label="0" value="0" />
 
-export default Buttons ;
+    <Button onClick={handleClick} label="/" value="/" />
+    <Button onClick={handleClick} label="8" value="8" />
+    <Button onClick={handleClick} label="5" value="5" />
+    <Button onClick={handleClick} label="2" value="2" />
+    <Button label="" value="null" />
+
+    <Button onClick={handleClick} label="x" value="*" />
+    <Button onClick={handleClick} label="9" value="9" />
+    <Button onClick={handleClick} label="6" value="6" />
+    <Button onClick={handleClick} label="3" value="3" />
+    <Button label="" value="null" />
+
+    <Button onClick={handleClick} label="-" value="-" />
+    <Button onClick={handleClick} label="+" size="2" value="+" />
+    <Button onClick={handleClick} label="=" size="2" value="equal" />
+    </div>
+
+)
+
+export default  Buttons;
